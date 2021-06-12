74d297abd9432c0aa39252352cd4a3431e292efc
Sarah Ciston <ciston@usc.edu>
2021-06-12 16:31:29 -0700

    first commit

--- _sv_custom.js
+++
@@ -1,8 +0,0 @@
-const { on } = require("node:events");
-
-!function($) {
-  // Write your own javascript here.
-  // $(window).resize(function(){
-  //   $("body").css({"background-size": "100% 100%"});
-  // })
-}(window.jQuery);
